bb61c9a3f7aaf520995c3b6f86f462ae7eb47cb9
duragindustries <69711522+duragindustries@users.noreply.github.com>
2020-09-26 22:52:13 +1200

    Updates

---
+++ precache-manifest.f8d509f2dfd6471e3df74d97e447d32a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93483b40d00f81298eddbf614868aea2",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "5a0bbe18c825b7bea96e",
+    "url": "/robofriends/static/css/main.27730405.chunk.css"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/robofriends/static/js/2.cab51068.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.cab51068.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a0bbe18c825b7bea96e",
+    "url": "/robofriends/static/js/main.09d1dac9.chunk.js"
+  },
+  {
+    "revision": "e7d2cbb368e0813da61d",
+    "url": "/robofriends/static/js/runtime-main.74eee7b5.js"
+  }
+]);
