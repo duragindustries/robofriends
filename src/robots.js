9f98585aaddeaea421f9484ab49725a2c29377a5
duragindustries <69711522+duragindustries@users.noreply.github.com>
2020-09-26 21:06:10 +1200

    update homepage

---
+++ src/robots.js
@@ -0,0 +1,26 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Anne',
+    username: 'Topopt',
+    email: 'anndurrantX@gmail.com'
+  },
+  {
+    id: 2,
+    name: 'Robert',
+    username: 'Bob',
+    email: 'bobcorpX@icloud.com'
+  },
+  {
+    id: 3,
+    name: 'Michael',
+    username: 'DuRag',
+    email: 'duragindustries@gmail.com'
+  },
+  {
+    id: 4,
+    name: 'Andrew',
+    username: 'Werdna',
+    email: 'XXdurrant@hotmail.com'
+  }
+];
